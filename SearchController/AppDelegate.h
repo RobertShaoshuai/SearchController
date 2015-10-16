d9fa44b18c519c95dd096f1855462011b4475051
Roberts <Roberts@robertsmacbook-pro.local>
2015-10-16 09:16:28 +0800

    Initial Commit

---
+++ SearchController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchController
+//
+//  Created by Roberts on 15/10/16.
+//  Copyright © 2015年 iBokan Wisdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchController
+//
+//  Created by Roberts on 15/10/16.
+//  Copyright © 2015年 iBokan Wisdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
